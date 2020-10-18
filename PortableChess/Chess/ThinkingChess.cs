a53c11def038365626259249f911876a1b5c7030
TechnologyLover <TechnologyLover@DESKTOP-193DU8E>
2020-10-18 17:12:57 +0330

    code completly repaired in castling;

--- PortableChess/Chess/ThinkingChess.cs
+++ PortableChess/Chess/ThinkingChess.cs
@@ -11019,6 +11019,9 @@ th.Dispose();
                 else
                                     if (kind == 6 && PenRegStrore && UsePenaltyRegardMechnisamT && PenaltyRegardListKing.Count == TableListKing.Count)
                     Equality = true;
+                else
+                                    if ((kind == 7 || kind == -7) && PenRegStrore && UsePenaltyRegardMechnisamT && PenaltyRegardListCastling.Count == TableListCastling.Count)
+                    Equality = true;
                 return Equality;
             }
         }
@@ -11045,6 +11048,9 @@ th.Dispose();
                     Equality = true;
                 else
                                      if (kind == 6 && Current.IsPenaltyAction() != 0 && UsePenaltyRegardMechnisamT && PenaltyRegardListKing != null && PenaltyRegardListKing.Count == TableListKing.Count)
+                    Equality = true;
+                else
+                                     if ((kind == 7||kind==-7) && Current.IsPenaltyAction() != 0 && UsePenaltyRegardMechnisamT && PenaltyRegardListCastling != null && PenaltyRegardListCastling.Count == TableListCastling.Count)
                     Equality = true;
                 return Equality;
             }
@@ -11079,6 +11085,10 @@ th.Dispose();
                                 if (kind == 6)
                     //King.
                     PenaltyRegardListKing.Add(Current);
+                else
+                                if (kind == 7||kind==-7)
+                    //King.
+                    PenaltyRegardListCastling.Add(Current);
             }
 
         }
@@ -11112,6 +11122,10 @@ th.Dispose();
                                 if (kind == 6)
                     //King.
                     PenaltyRegardListKing.RemoveAt(PenaltyRegardListKing.Count - 1);
+                else
+                                if (kind == 7||kind==-7)
+                    //King.
+                    PenaltyRegardListCastling.RemoveAt(PenaltyRegardListCastling.Count - 1);
             }
         }
         //learning autamata maib method
@@ -15563,6 +15577,20 @@ th.Dispose();
                 }
             }
             else if (kind == 6)//king
+            {
+                //when valid do create of deeper node and string making
+                if (TableListKing.Count > AStarGreedy.Count)
+                {
+                    if (AStarGreedy.Count == 0)
+                        AStarGreedy = new List<AllDraw>();
+                    AStarGreedy.Add(new AllDraw(Order * -1, MovementsAStarGreedyHeuristicFoundT, IgnoreSelfObjectsT, UsePenaltyRegardMechnisamT, BestMovmentsT, PredictHeuristicT, OnlySelfT, AStarGreedyHeuristicT, ArrangmentsChanged));
+                    AStarGreedy[AStarGreedy.Count - 1].TableList.Clear();
+                    AStarGreedy[AStarGreedy.Count - 1].TableList.Add(CloneATable(TableListKing[j]));
+                    AStarGreedy[AStarGreedy.Count - 1].SetRowColumn(0);
+                    AStarGreedy[AStarGreedy.Count - 1].AStarGreedyString = THIS;
+                }
+            }
+            else if (kind == 7||kind==-7)//king
             {
                 //when valid do create of deeper node and string making
                 if (TableListKing.Count > AStarGreedy.Count)

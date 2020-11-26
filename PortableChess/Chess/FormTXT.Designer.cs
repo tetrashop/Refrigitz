1f28a3439aa22d40a06ee1696ba78280dcc4f752
TechnologyLover <TechnologyLover@DESKTOP-193DU8E>
2020-11-27 00:02:32 +0330

    color tree view indicator;

--- PortableChess/Chess/FormTXT.Designer.cs
+++ PortableChess/Chess/FormTXT.Designer.cs
@@ -35,6 +35,14 @@ namespace Refrigtz
             this.contextMenuStripRefrigitzTree = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.showTreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backgroundWorkertreeView = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.contextMenuStripRefrigitzTree.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +52,7 @@ namespace Refrigtz
             this.TextBoxTXT.Multiline = true;
             this.TextBoxTXT.Name = "TextBoxTXT";
             this.TextBoxTXT.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.TextBoxTXT.Size = new System.Drawing.Size(1307, 563);
+            this.TextBoxTXT.Size = new System.Drawing.Size(1307, 529);
             this.TextBoxTXT.TabIndex = 0;
             this.TextBoxTXT.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
@@ -52,7 +60,7 @@ namespace Refrigtz
             // 
             this.treeViewRefregitzDraw.Location = new System.Drawing.Point(12, 13);
             this.treeViewRefregitzDraw.Name = "treeViewRefregitzDraw";
-            this.treeViewRefregitzDraw.Size = new System.Drawing.Size(1308, 563);
+            this.treeViewRefregitzDraw.Size = new System.Drawing.Size(1308, 529);
             this.treeViewRefregitzDraw.TabIndex = 85;
             this.treeViewRefregitzDraw.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewRefregitzDraw_AfterSelect);
             this.treeViewRefregitzDraw.BindingContextChanged += new System.EventHandler(this.treeViewRefregitzDraw_BindingContextChanged);
@@ -76,11 +84,99 @@ namespace Refrigtz
             // 
             this.backgroundWorkertreeView.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkertreeView_DoWork);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(26, 563);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 86;
+            this.label1.Text = "LoseOcuuredatChiled[0] < 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(171, 563);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 87;
+            this.label2.Text = "Draw.IsCurrentDraw";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(279, 563);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 88;
+            this.label3.Text = "IsThereMateOfSelf[j]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(389, 563);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 89;
+            this.label4.Text = "IsThereMateOfEnemy[j]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(514, 563);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 90;
+            this.label5.Text = "KishEnemy[j]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(588, 562);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 91;
+            this.label6.Text = "KishSelf[j]";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Gray;
+            this.label7.Location = new System.Drawing.Point(647, 563);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 92;
+            this.label7.Text = "HaveKilled > 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Brown;
+            this.label8.Location = new System.Drawing.Point(729, 562);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 13);
+            this.label8.TabIndex = 93;
+            this.label8.Text = "HaveKilled < 0";
+            // 
             // FormTXT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1332, 588);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.treeViewRefregitzDraw);
             this.Controls.Add(this.TextBoxTXT);
             this.Name = "FormTXT";
@@ -100,5 +196,13 @@ namespace Refrigtz
         private System.Windows.Forms.ContextMenuStrip contextMenuStripRefrigitzTree;
         private System.Windows.Forms.ToolStripMenuItem showTreeToolStripMenuItem;
         private System.ComponentModel.BackgroundWorker backgroundWorkertreeView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }

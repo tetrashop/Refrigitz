6b0177105e7002e502f911484d53a5c22f0a0087
tetrashop <ramin.edjlal1359@gmail.com>
2021-01-09 23:07:27 +0330

    coloring;

--- Chess/Chess.Designer.cs
+++ Chess/Chess.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chess));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -36,7 +37,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(11, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 258);
+            this.button1.Size = new System.Drawing.Size(124, 258);
             this.button1.TabIndex = 0;
             this.button1.Text = "White";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,21 +45,26 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Location = new System.Drawing.Point(150, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(131, 258);
             this.button2.TabIndex = 1;
             this.button2.Text = "Black";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Chess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Chess";
             this.Text = "Chess";
             this.ResumeLayout(false);

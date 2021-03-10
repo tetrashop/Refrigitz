041c8837b66cf0af7ec59f262b5ea3622f1455c6
tetrashop <ramin.edjlal1359@gmail.com>
2021-03-10 18:20:32 +0330

    AppRefrigitz initiates;

---
+++ AppRefrigitz/AppRefrigitz/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppRefrigitz.Services;
+using AppRefrigitz.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppRefrigitz
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

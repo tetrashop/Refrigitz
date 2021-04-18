9cde48243e96c7c857a74bb80e5c8f4aba670c92
tetrashop <ramin.edjlal1359@gmail.com>
2021-04-18 21:18:56 +0430

    take give proposed;

--- ImageTextDeepLearning/ResultsOfSupposed.cs
+++ ImageTextDeepLearning/ResultsOfSupposed.cs
@@ -8,6 +8,96 @@ namespace ContourAnalysisNS
 {
     public class ResultsOfSupposed
     {
+        public static List<string> mined = new List<string>();
+        public static bool MindedIsVerb(List<string> te, List<List<string>> telo)
+        {
+            bool Is = false;
+
+            for (int p = 0; p < telo.Count; p++)
+            {
+                for (int q = 0; q < telo.Count; q++)
+                {
+                    if (p == q)
+                        continue;
+
+                    for (int r = 0; r < telo.Count; r++)
+                    {
+                        if (p == r)
+                            continue;
+                        if (q == r)
+                            continue;
+
+                        for (int s = 0; s < telo.Count; s++)
+                        {
+                            if (s == p)
+                                continue;
+                            if (s == q)
+                                continue;
+                            if (s == r)
+                                continue;
+
+
+                        }
+                    }
+                }
+            }
+            return Is;
+        }
+        public static List<string> MindedIsVerb(List<string> te, List<List<string>> telo,bool p,bool q,bool r,bool s)
+        {
+            string se = "";
+            List<string> Strlogic = new List<string>();
+            PandPgivesQIsQ(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PgivesQandQgivesRIsPgivesR(p, q, r, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PgivesQandnotQIsnotP(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PQIsPANDQ(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            Contradiction(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            ContradictionSample(p, q, r, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PandQ(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PorQandnotPgivesQ(p, q, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PQandPgivesQgivesRisR(p, q, r, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PgivesQandQgivesRisPOrQgivesR(p, q, r, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PgivesQandRgivesSandPorRIsQorS(p, q, r, s, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            PgivesQandRgivesSandnotPornotRIsnotPornotQ(p, q, r, s, ref se);
+            Strlogic.Add(se);
+            se = "";
+
+            return Strlogic;
+
+        }
         public static bool PandPgivesQIsQ(bool p, bool q, ref string Re)
         {
             bool s = p && ((!p) || q);

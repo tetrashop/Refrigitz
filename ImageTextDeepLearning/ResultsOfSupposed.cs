dd05661813b6cce633a548bdb518b810a6eb5346
tetrashop <ramin.edjlal1359@gmail.com>
2021-04-18 11:36:49 +0430

    ResultsOfSupposed; database of knowledge;

---
+++ ImageTextDeepLearning/ResultsOfSupposed.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContourAnalysisNS
+{
+    public class ResultsOfSupposed
+    {
+        public static bool PandPgivesQIsQ(bool p, bool q, ref string Re)
+        {
+            bool s = p && ((!p) || q);
+            if (s)
+            {
+                Re = "q";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PgivesQandQgivesRIsPgivesR(bool p, bool q, bool r, ref string Re)
+        {
+            bool s = ((!p) || q) && ((!p) || r);
+            if (s)
+            {
+                Re = "p->r";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PgivesQandnotQIsnotP(bool p, bool q, ref string Re)
+        {
+            bool s = ((!p) || q) && ((!q));
+            if (s)
+            {
+                Re = "~p";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PQIsPANDQ(bool p, bool q, ref string Re)
+        {
+            bool s = (p) && (q);
+            if (s)
+            {
+                Re = "p&&Q";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool Contradiction(bool s, bool f, ref string Re)
+        {
+            bool a = (s) || (f);
+            if (a)
+            {
+                Re = "S";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool ContradictionSample(bool p, bool q, bool f, ref string Re)
+        {
+            bool a = ((!(p && (!q))) || (f));
+            if (a)
+            {
+                Re = "p->q";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PandQ(bool p, bool q, ref string Re)
+        {
+            bool a = (p && q);
+            if (a)
+            {
+                Re = "p";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PorQandnotPgivesQ(bool p, bool q, ref string Re)
+        {
+            bool a = (p || q) && (!p);
+            if (a)
+            {
+                Re = "q";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PQandPgivesQgivesRisR(bool p, bool q, bool r, ref string Re)
+        {
+            bool a = (p && q) && (((!p) || ((!q) || r)));
+            if (a)
+            {
+                Re = "r";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PgivesQandQgivesRisPOrQgivesR(bool p, bool q, bool r, ref string Re)
+        {
+            bool a = ((!p) || q) && ((!q) || r);
+            if (a)
+            {
+                Re = "p||q->r";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PgivesQandRgivesSandPorRIsQorS(bool p, bool q, bool r, bool s, ref string Re)
+        {
+            bool a = ((!p) || q) && ((!r) || s) && (p || q);
+            if (a)
+            {
+                Re = "q||s";
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool PgivesQandRgivesSandnotPornotRIsnotPornotQ(bool p, bool q, bool r, bool s, ref string Re)
+        {
+            bool a = ((!p) || q) && ((!r) || s) && ((!q) || (!!s));
+            if (a)
+            {
+                Re = "!p||!q";
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
